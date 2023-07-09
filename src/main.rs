3f3f0df5c9e3be798293515d1b6663cf62347b8e
David-Trantham <33731366+David-Trantham@users.noreply.github.com>
2023-07-09 16:14:03 -0400

    Working towards file read/write

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
-// use same_file::Handle;
+
+use std::env::consts;
 use std::fs::File;
-use std::io::{BufRead,BufReader,Error,ErrorKind};
+use std::io::{Write, BufRead,BufReader, BufWriter, Error};
 use std::path::Path;
 
+fn initialize_output() -> Result<(), Error> {
+    
+}
 fn main() -> Result<(), Error> {
-    let path_to_read = Path::new("test.txt");
+    let input_path = Path::new("test.txt");
+    let output_path = Path::new("output.txt");
+    let mut buffer = Vec::new;
+    let buffer = BufWriter::new(buffer);
 
-    // let stdout_handle = Handle::stdout()?;
-    // let handle = Handle::from_path(path_to_read)?;
+    // let file = File::open(&input_path)?;
+    // let file = BufReader::new(file);
+    // for (num, line) in file.lines().enumerate() {
+    //     println!("{} : {}", num, line?.to_uppercase());
+    let mut output = File::create(output_path)?;
+    write!(output, "initialize")?;
 
-    // if stdout_handle == handle {
-    //     return Err(Error::new(
-    //         ErrorKind::Other,
-    //         "You are reading and writing from the same file"
-    //     ));
-    // } else {
-        let file = File::open(&path_to_read)?;
-        let file = BufReader::new(file);
-        for (num, line) in file.lines().enumerate() {
-            println!("{} : {}", num, line?.to_uppercase());
-        // }
+    let input = File::open(input_path)?;
+    let input = BufReader::new(input);
+    const CORRECT_ANSWER_ID: &str = "( Missed)";
+    const END_OF_QUESTION: &str = "Your answer to this question is incorrect";
+    for (num, line) in input.lines().enumerate(){
+        let this_line = line?;
+        //If we are still "Within a single question"
+        if !this_line.starts_with(END_OF_QUESTION) {
+            write!(buffer, "hi there");
+        }
+
     }
     Ok(())
 }
