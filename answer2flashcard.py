8118470db94e4def339c824c4917ffc8fef8918a
David-Trantham <33731366+David-Trantham@users.noreply.github.com>
2023-07-11 15:32:49 -0400

    Bug fixes

--- answer2flashcard.py
+++ answer2flashcard.py
@@ -30,7 +30,7 @@ def first_answer(filepath):
         print(question + "\n" + answer + "end")
 
 def all_answers(filepath):
-    anki_formatted_quiz = ""
+    anki_formatted_quiz = "\n"
     quiz = open(filepath, "r")
     quiz.readline() #Have to skip the first line
     thisline = quiz.readline().strip()
@@ -51,7 +51,7 @@ def all_answers(filepath):
 #I would like this function to accept a filepath and create an output with name based on the tag
 def quiz2anki(filepath):
     print(filepath)
-    output_path = initialize_output()
+    output_path = initialize_output(filepath)
     output_file = open(output_path, "a")
     output_file.write(all_answers(filepath))
     
