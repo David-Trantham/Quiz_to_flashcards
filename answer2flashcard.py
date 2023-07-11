08a8e9e378ff3b6f0be6e04288bfebef230fd9bc
David-Trantham <33731366+David-Trantham@users.noreply.github.com>
2023-07-11 14:10:11 -0400

    Add functionality for file initialization and answer finding

---
+++ answer2flashcard.py
@@ -0,0 +1,31 @@
+import os
+QUIZ = "Exam 1001 practice quiz.txt"
+ANSWER_IDENTIFIER = " ( Missed)\n"
+
+def initialize_output():
+    #TODO: Could also use the tag for filename
+    #Open file containing answers and get tag from first line
+    get_name = open(QUIZ, "r")
+    firstline = get_name.readline()
+    tag = firstline.split(" - ")[1].strip()
+    get_name.close()
+    #generate output file called output.txt
+    new_output = open(r"output.txt", "w")
+    new_output.write("tags:" + tag)
+    new_output.close()
+    
+def first_answer():
+    question = ""
+    answer = ""
+    quiz = open(QUIZ, "r")
+    quiz.readline() #Have to skip the first line
+    thisline = quiz.readline().strip()
+    while (not(thisline.startswith("Your answer"))):
+        if thisline.endswith(ANSWER_IDENTIFIER):
+             thisline = thisline.removesuffix(ANSWER_IDENTIFIER) + "\n"
+             answer += thisline
+        question += thisline
+        thisline = quiz.readline()
+        print(question + "\n" + answer + "end")
+
+
